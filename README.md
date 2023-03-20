3a4e2eed26e93686b75a241cb0706e240f93ff19
Krutagna Patel <kpatel.kp7016@gmail.com>
2023-03-20 10:31:19 +0530

    readme updated

--- README.md
+++ README.md
@@ -2,11 +2,16 @@
 
 ### Screenshot
 
-![](screenshot/Screenshot%20.png);
+![](screenshot/Screenshot%20.png)
 
 ### Links
 
 - Live Site URL: [https://login-app-10.vercel.app/]
+
+### What I Learned
+- useEffect
+- useReducer
+- useContext
 
 ### Built with
 
--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,12 +1,11 @@
 import "./Button.css";
 
-const Button = ({ className, children, onClick, type, disabled }) => {
+const Button = ({ className, children, onClick, type }) => {
   return (
     <button
       className={`btn ${className}`}
       onClick={onClick}
       type={type === "submit" ? "submit" : "button"}
-      disabled={disabled}
     >
       {children}
     </button>
